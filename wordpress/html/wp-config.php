3e75e2644e52bbf46364e5becee2d98bc4a522a9
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-03-14 21:48:48 -0300

    added sonarqube server setup files

--- wordpress/html/wp-config.php
+++
@@ -1,6 +0,0 @@
-if ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
-    $_SERVER['HTTPS'] = '1';
-
-if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
-}
